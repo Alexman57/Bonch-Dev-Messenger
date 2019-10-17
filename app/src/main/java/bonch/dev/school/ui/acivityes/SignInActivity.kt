7ffef7a800583d8654d4933a0daf195bf9f503d6
Алексей <gaponenco57@gmail.com>
2019-10-17 22:28:26 +0300

    start

---
+++ app/src/main/java/bonch/dev/school/ui/acivityes/SignInActivity.kt
@@ -0,0 +1,13 @@
+package bonch.dev.school.ui.acivityes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bonch.dev.school.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
